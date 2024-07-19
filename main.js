77bc3eb797422787b8f376986dd6a384a4e4653c
being2 <129958950+beingtwo@users.noreply.github.com>
2024-07-19 21:00:44 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+
+
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { Player } = require('discord-player');
+const express = require('express');
+require('dotenv').config();
+
+
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const { printWatermark } = require('./functions/handlers');
+const configPath = './config.json';
+const configData = JSON.parse(fs.readFileSync(configPath));
+const prefix = configData.prefix;
+
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+});
+printWatermark();
+
+<<<<<<< HEAD
+client.commands = new Map();
+=======
+client.commands = new Map();
+>>>>>>> a4605c8cb8cc95c9a351e5f51f4ba5ba8ca7ca0c
+
+client.on('messageCreate', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName);
+
+  if (!command) return;
+
+  try {
+    command.execute(message, args);  
+  } catch (error) {
+      console.error(error);
+      message.reply('There was an error trying to execute that command!');
+  }
+});
+
+
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log('\x1b[32m%s\x1b[0m', '|    🍔 Bot logged in successfully!');
+    console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
+    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+    console.log('\x1b[36m%s\x1b[0m', `|    🏡 Bot is in ${client.guilds.cache.size} servers`);
+  } catch (error) {
+    console.error('\x1b[31m%s\x1b[0m', '❌ Failed to log in:', error);
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Login, Restarting Process...');
+    process.kill(1);
+  }
+}
+
+
+client.once('ready', () => {
+  setTimeout(() => {
+    console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
+    client.user.setPresence({
+      activities: [{ name: `Cool Music Thingy`, type: ActivityType.Playing }],
+      status: 'online',
+    });
+  }, 2000); 
+});
+
+
+login();
+
+
+setInterval(() => {
+  if (!client || !client.user) {
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Logged in, Restarting Process...');
+    process.kill(1);
+  }
+}, 15000);
+
+module.exports = client;
+
+
